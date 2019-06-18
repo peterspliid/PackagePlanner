adcf62344c9d2632e5443269509498db8cbe8062
Jan Svenssonrr <jsv@netcompany.crgom>
2019-06-18 16:45:22 +0200

    Implement basic layout

---
+++ PackagePlanner/Scripts/home.js
@@ -0,0 +1,9 @@
+﻿var button = document.getElementById("go-button");
+var list = document.getElementById("results-list");
+
+list.style.visibility = "hidden";
+
+button.onclick = function() {
+    list.style.visibility = "visible";
+}
+
