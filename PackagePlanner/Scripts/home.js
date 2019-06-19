b1ce2b781c4aa4591eec5ecc41b54df762f20a08
Jan Svenssonrr <jsv@netcompany.crgom>
2019-06-19 08:11:50 +0200

    Add spinner when loading results

--- PackagePlanner/Scripts/home.js
+++ PackagePlanner/Scripts/home.js
@@ -1,9 +1,43 @@
-﻿var button = document.getElementById("go-button");
-var list = document.getElementById("results-list");
+﻿const list = document.getElementById("results-list");
+list.style.visibility = "hidden";
+
+const spinner = document.getElementById("spinner")
+spinner.style.visibility = 'hidden';
+
+var resultsFetched = false;
+
+var startSpinner = function () {
+    spinner.style.visibility = 'visible';
+}
+
+var stopSpinner = function() {
+    spinner.style.visibility = 'hidden';
+}
+
+var fetchResults = function (completion) {
+    if (resultsFetched) {
+        return;
+    }
+
+    startSpinner();
+
+    // Database code
+    setTimeout(stopSpinner, 2000);
+    setTimeout(function() {
+        list.style.visibility = "visible";
+    }, 2000)
+    resultsFetched = true;
+}
 
 list.style.visibility = "hidden";
 
-button.onclick = function() {
-    list.style.visibility = "visible";
+var button = document.getElementById("go-button");
+
+button.onclick = function () {
+    if (resultsFetched) {
+        return;
+    }
+
+    fetchResults();
 }
 
