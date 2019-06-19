78d6b7ea6079f20e39d4821389938d0b105a3220
Jan Svenssonrr <jsv@netcompany.crgom>
2019-06-19 10:19:44 +0200

    Make styling and cleanup changes

--- PackagePlanner/Controllers/HomeController.cs
+++ PackagePlanner/Controllers/HomeController.cs
@@ -12,11 +12,5 @@ namespace PackagePlanner.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-            return View();
-        }
     }
 }
--- PackagePlanner/Scripts/home.js
+++ PackagePlanner/Scripts/home.js
@@ -1,6 +1,9 @@
 ﻿const list = document.getElementById("results-list");
 list.style.visibility = "hidden";
 
+const errorView = document.getElementById("error-view");
+errorView.style.display = 'none';
+
 const spinner = document.getElementById("spinner")
 spinner.style.visibility = 'hidden';
 
@@ -21,6 +24,7 @@ var stopSpinner = function() {
 
 var fetchResults = function (completion) {
     list.style.visibility = "hidden";
+    errorView.style.display = 'none';
     isFetchingResults = true;
     startSpinner();
 
@@ -32,12 +36,28 @@ var fetchResults = function (completion) {
 
     console.log(`From: ${from}, To: ${to}, Weight: ${weight}, customerId: ${customerId}`);
 
-    var fastestTimeLabel;
-    var fastestPriceLabel;
-    var bestTimeLabel;
-    var bestPriceLabel;
-    var cheapestTimeLabel;
-    var cheapestPriceLabel;
+    const fastestTimeLabel = document.getElementById("fastest-time");
+    const fastestPriceLabel = document.getElementById("fastest-price");
+    const bestTimeLabel = document.getElementById("best-time");
+    const bestPriceLabel = document.getElementById("best-price");
+    const cheapestTimeLabel = document.getElementById("cheapest-time");
+    const cheapestPriceLabel = document.getElementById("cheapest-price");
+
+    fastestTimeLabel.innerHTML = "8";
+    fastestPriceLabel.innerHTML = "15";
+    bestTimeLabel.innerHTML = "8";
+    bestPriceLabel.innerHTML = "15";
+    cheapestTimeLabel.innerHTML = "24";
+    cheapestPriceLabel.innerHTML = "12";
+
+
+    if (customerId === "") {
+        errorView.innerHTML = "No customer id provided"
+        errorView.style.display = 'block';
+        stopSpinner();
+        isFetchingResults = false;
+        return;
+    }
 
     setTimeout(stopSpinner, 2000);
     setTimeout(function() {
@@ -48,7 +68,7 @@ var fetchResults = function (completion) {
 
 list.style.visibility = "hidden";
 
-var button = document.getElementById("go-button");
+const button = document.getElementById("go-button");
 
 button.onclick = function () {
     if (isFetchingResults) {
@@ -56,4 +76,19 @@ button.onclick = function () {
     }
 
     fetchResults();
+}
+
+const selectBestButton = document.getElementById("select-best-button");
+selectBestButton.onclick = function (e) {
+    console.log("Best clicked");
+}
+
+const selectFastestButton = document.getElementById("select-fastest-button");
+selectFastestButton.onclick = function(e) {
+    console.log("Fastest clicked");
+}
+
+const selectCheapestButton = document.getElementById("select-cheapest-button");
+selectCheapestButton.onclick = function(e) {
+    console.log("Cheapest clicked");
 }
