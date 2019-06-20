7d76cc2e4ec36ea35dadf66af8fcd374e4003455
Peter Spliid <pesp@netcompany.com>
2019-06-20 10:13:32 +0200

    variable names

---
+++ PackagePlanner/Controllers/ApiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using PackagePlanner.Models;
+
+namespace PackagePlanner.Controllers
+{
+    public class APIController : ApiController
+    {
+        public DeliveryApi Get()
+        {
+            return new DeliveryApi(true, 20, 16);
+        }
+    }
+}
---
+++ PackagePlanner/Models/DeliveryApi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Management;
+
+namespace PackagePlanner.Models
+{
+    public class DeliveryApi
+    {
+        public bool success;
+        public int price;
+        public int time;
+
+        public DeliveryApi(bool Success, int Price, int Time)
+        {
+            success = Success;
+            price = Price;
+            time = Time;
+        }
+    }
+}
