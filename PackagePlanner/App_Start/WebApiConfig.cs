058780a981fd87a2c8f596ec14acfb2431c1eb1a
Peter Spliid <pesp@netcompany.com>
2019-06-20 12:54:56 +0200

    Unit tests

--- PackagePlanner/App_Start/WebApiConfig.cs
+++ PackagePlanner/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace PackagePlanner
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "{controller}/{id}",
+                routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ PackagePlannerTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace PackagePlanner.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
