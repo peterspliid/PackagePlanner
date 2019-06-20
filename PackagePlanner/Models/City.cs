545f31105e3d0185cb95d43d4b7f421ba744959a
NCDMZ\rea <rea@netcompany.com>
2019-06-20 20:19:40 +0200

    Created model City

---
+++ PackagePlanner/Models/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace PackagePlanner.Models
+{
+    public class City
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public City(DataRow row)
+        {
+            Id = (string)row[0];
+            Name = (string)row[1];
+        }
+    }
+}
