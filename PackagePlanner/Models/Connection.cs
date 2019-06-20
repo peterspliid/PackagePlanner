0ebaa04cecef7d20246f1f71891c0c3d11cd2103
Peter Spliid <pesp@netcompany.com>
2019-06-20 15:01:14 +0200

    Added dijkstras algorithm

---
+++ PackagePlanner/Models/Connection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PackagePlanner.Models
+{
+    public class Connection
+    {
+        public int Id { get; set; }
+        public string Place1 { get; set; }
+        public string Place2 { get; set; }
+        public string ConnectionType { get; set; }
+    }                              
+}
---
+++ PackagePlannerTests/Utilities/AlgorithmDijkstraAirOnlyTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PackagePlanner.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackagePlanner.Utilities.Tests
+{
+    [TestClass()]
+    public class AlgorithmDijkstraAirOnlyTests
+    {
+        [TestMethod()]
+        public void GetPriceFromOceanicAirlinesAPITest()
+        {
+            Assert.Fail();
+        }
+    }
+}
