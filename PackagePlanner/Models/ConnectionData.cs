6ddbc672b33423e613ff95452906f49d88f4d628
Peter Spliid <pesp@netcompany.com>
2019-06-20 21:32:25 +0200

    Initial dijksta algorithm for input parameters

---
+++ PackagePlanner/Models/ConnectionData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PackagePlanner.Models
+{
+    public class ConnectionData
+    {
+        public int Weight { get; set; }
+        public string Type { get; set; }
+    }
+}
--- PackagePlanner/Utilities/ShortestPathCalculator.cs
+++ PackagePlanner/Utilities/ShortestPathCalculator.cs
@@ -5,11 +5,39 @@ using System.Web;
 
 namespace PackagePlanner.Utilities
 {
-    public class ShortestPathCalculator
+    public static class ShortestPathCalculator
     {
-        public void ShortestPathFlight()
+        public static List<Models.Connection> connections;
+        public static List<Models.Connection> flightConnections;
+        public static List<string> cities;
+        public static Dictionary<string, Models.ConnectionData> oaConnectionsData;
+        static ShortestPathCalculator()
         {
+            //connections = Database.Instance.GetConnections();
+            //flightConnections = connections.Where(cn => cn.ConnectionType == "oa").ToList();
+            //cities = Database.Instance.GetCities();
+            //oaConnectionsData = new Dictionary<string, Models.ConnectionData>();
 
+            //foreach(Models.Connection con in connections)
+            //{
+            //    if (con.ConnectionType == "oa")
+            //    {
+            //        Models.ConnectionData conData = new Models.ConnectionData
+            //        {
+            //            Weight = 1,
+            //            Type = "oa"
+            //        };
+            //        oaConnectionsData[($"{con.Place1}-{con.Place2}")] = conData;
+            //        oaConnectionsData[($"{con.Place2}-{con.Place1}")] = conData;
+            //    }
+            //}
+
+
+        }
+        public static void ShortestPathFlight(Models.ApiRequestParams apiRequestParams)
+        {
+            Dictionary<string, Models.ConnectionData> connectionsData = new Dictionary<string, Models.ConnectionData>();
+            
         }
     }
 }
