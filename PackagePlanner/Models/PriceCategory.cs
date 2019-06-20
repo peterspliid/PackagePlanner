3d5f6b2a2730b304a89e557030d6bc414d5e988d
NCDMZ\rea <rea@netcompany.com>
2019-06-20 20:47:57 +0200

    Added model PriceCategory

---
+++ PackagePlanner/Models/PriceCategory.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+
+namespace PackagePlanner.Models
+{
+    public class PriceCategory
+    {
+        public string Id { get; set; }
+        public string CargoSizeCategoryId { get; set; }
+        public string WeightCategoryId { get; set; }
+        public double Price { get; set; }
+        public string Currency { get; set; }
+
+        public PriceCategory(DataRow row)
+        {
+            Id = (string)row[0];
+            CargoSizeCategoryId = (string)row[1];
+            WeightCategoryId = (string)row[2];
+            Price = (double)row[3];
+            Currency = (string)row[4];
+        }
+    }
+}
