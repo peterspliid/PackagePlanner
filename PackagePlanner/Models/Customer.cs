15d2865f093f7c63b6280c978bc87bb80492f3a7
NCDMZ\rea <rea@netcompany.com>
2019-06-20 22:53:46 +0200

    added new models and set and get functions to write to database

---
+++ PackagePlanner/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+
+namespace PackagePlanner.Models
+{
+    public class Customer
+    {
+        public string id { get; set; }
+        public string Name { get; set; }
+
+        public Customer(DataRow row)
+        {
+            id = (string)row[0];
+            Name = (string)row[1];
+        }
+    }
+}
---
+++ PackagePlanner/Models/PlannedPackage.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+
+namespace PackagePlanner.Models
+{
+    public class PlannedPackage
+    {
+        public string Id { get; set; }
+        public string CustomerId { get; set; }
+        public string CargoTypeId { get; set; }
+        public double Price { get; set; }
+        public double DeliveryTime { get; set; }
+        public double Discount { get; set; }
+        public string FromCityId { get; set; }
+        public string ToCityId { get; set; }
+        public string PriceCategoryId { get; set; }
+        public double PackageHight { get; set; }
+        public double PackageWidth { get; set; }
+        public double PackageLength { get; set; }
+
+        public PlannedPackage(DataRow row)
+        {
+            Id = (string)row[0];
+            CustomerId = (string)row[1];
+            CargoTypeId = (string)row[2];
+            Price = (double)row[3];
+            DeliveryTime = (double)row[4];
+            Discount = (double)row[5];
+            FromCityId = (string)row[6];
+            ToCityId = (string)row[7];
+            PriceCategoryId = (string)row[8];
+            PackageHight = (double)row[9];
+            PackageWidth = (double)row[10];
+            PackageLength = (double)row[11];
+        }
+    }
+}
