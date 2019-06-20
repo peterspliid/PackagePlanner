b08bf89b55c4b695e06e424388eec3d73f4ae5ec
Thor Winther <thw@netcompany.com>
2019-06-20 14:17:37 +0200

    Created api call to oceanic airlines

--- PackagePlanner/Utilities/APIHandling.cs
+++ PackagePlanner/Utilities/APIHandling.cs
@@ -1,28 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web;
 using PackagePlanner.Models;
+using Newtonsoft.Json;
 
 namespace PackagePlanner.Utilities
 {
     public static class APIHandling
     {
-        static HttpClient client = new HttpClient();
 
-
-        static async Task<DeliveryApi> GetProductAsync(string path)
+        public static int GetPriceFromOceanicAirlinesAPI()
         {
-            DeliveryApi delivery = null;
-            HttpResponseMessage response = await client.GetAsync(path);
-            if (response.IsSuccessStatusCode)
-            {
-                delivery = await response.Content.ReadAsAsync<DeliveryApi>();
-            }
-            return delivery;
+            var url = "http://wa-oadk.azurewebsites.net/api";
+            DeliveryApi delivery = APIHandling.AsyncTaskCallWebApiAsync(url);
+
+            return delivery.price;
         }
+
+        public static int GetPriceFromTelstarAPI()
+        {
+            var url = "http://wa-tldk.azurewebsites.net/api/GetTelstarRouteExternal?";
+
+            DeliveryApi delivery = APIHandling.AsyncTaskCallWebApiAsync(url);
+
+            return delivery.price;
+        }
+
+        public static int GetPriceFromEITcompanyAPI()
+        {
+            var url = "http://wa-oadk.azurewebsites.net/api";
+            DeliveryApi delivery = APIHandling.AsyncTaskCallWebApiAsync(url);
+
+            return delivery.price;
+        }
+
+        public static DeliveryApi AsyncTaskCallWebApiAsync(string url)
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Clear();  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));  
+                //GET Method  
+                HttpResponseMessage response = client.GetAsync(("api")).Result;  
+                if (response.IsSuccessStatusCode)  
+                {
+                    DeliveryApi delivery = response.Content.ReadAsAsync<DeliveryApi>().Result;  
+                    Console.WriteLine("Id:{0}\tName:{1}", delivery.price, delivery.time);
+                    return delivery;
+                }  
+                else
+                {
+                    Console.WriteLine("Internal server Error");
+                }
+                return null;
+
+        } 
+          
+
 
     }
 
