064ca1aafb2b8478ccec213531b8eeab3ebda0de
Thor Winther <thw@netcompany.com>
2019-06-20 13:04:01 +0200

    Added extra classes to support the air onl algorithm

---
+++ PackagePlanner/Utilities/APIHandling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using PackagePlanner.Models;
+
+namespace PackagePlanner.Utilities
+{
+    public static class APIHandling
+    {
+        static HttpClient client = new HttpClient();
+
+
+        static async Task<DeliveryApi> GetProductAsync(string path)
+        {
+            DeliveryApi delivery = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                delivery = await response.Content.ReadAsAsync<DeliveryApi>();
+            }
+            return delivery;
+        }
+
+    }
+
+}
---
+++ PackagePlanner/Utilities/AlgorithmDijkstraAirOnly.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PackagePlanner.Utilities
+{
+    public static class AlgorithmDijkstraAirOnly
+    {
+        public static int GetPriceFromOceanicAirlinesAPI()
+        {
+            // Make sure you call API
+            return 25;
+        }
+    }
+}
